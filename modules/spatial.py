ff46bae2fd862ebb13db874d68aa23b18201c773
Stephen Northey <67577807+sanorthey@users.noreply.github.com>
2024-09-12 04:50:13 +1000

    Significant Optimisation - coordinate generation
    Prior implementation was resulting in 'iteration execution duration' in excess of 20 seconds when testing input file examples using input_geopackage.gpkg with 3-code ISO country labels. Significantly restructured regional spatial data handling to return this to under 0.1 seconds.
    
    Key Changes:
    - Geopackage region filtering only done once when importing input_exploration_production_factors.csv
    - Individual geodatabase now assigned to (region, deposit_type) keys stored in the exploration_production_factors data structure (i.e. imported_factors, f[factor][index])
    - As part of this, precomputing polygon bounds, generating spatial index trees and converting to 'prepared regions' to enable very fast and highly cached generated coordinate bounds checking.
    - Optional functionality also implemented (but ignored currently) for polygon simplification.
    
    Implementation Notes:
    - 'prepared regions' are unable to be pickled and so can't be passed to the multiprocessing pools or to deepcopy. Pre-computation therefore done at the start of pemmss.scenario() before the iteration loop. Then updated into the imported_factors data structure after the iteration loop. Probably best to avoid modifying this overall implementation logic without good reason, as this is key to sub-second iteration performance.
    
    ------
    
    pemmss.scenario()
    - gdf_prepared_list variable generated then updated in factors at each iteration.
    - local imported_geopackage_gdf variable removed as no longer directly passed as argument to import_projects() or resource_discovery()
    
    modules.spatial.py
    - Added import of shapely.strtree.STRtree
    - Added import of shapely.prepared.prep
    - Removed import of shapely.ops.nearest_points
    - Updated some docstrings
    - Created function gdf_region_preprocess()
    - Created function prepare_gdf()
    
    modules.spatial.generate_region_coordinate()
    - Separated geodatabase filtering logic into spatial.gdf_region_preprocess() and file_import.import_exploration_production_factors()
    - Moved bound derivation logic to spatial.gdf_region_preprocess()
    - Optimised random bound checks for multi-polygon regions
    - Removed midpoint and centroid_within methods as these are unlikely to be used
    
    modules.spatial.deduplicate_columns()
    - No longer using shapefiles, so removed the 10 character column name limit.
    
    modules.spatial.save_scenario_geopackage()
    - Variable names and comments related to shapefiles renamed to reference geodataframe.
    - Code unchanged
    
    modules.file_import.import_static_files()
    - Importing geopackage earlier then passing to exploration_production_factor import to enable one pass mapping of regions.
    
    modules.file_import.import_projects()
    - geodataframe keyword argument removed (now embedded within f data structure)
    - geodataframe log message simplified
    - Empty geodataframe checking logic moved to spatial.gdf_region_preprocess() and result available through factors data structure
    
    modules.file_import.import_exploration_production_factors()
    - Added 'geodataframe' keyword argument
    - return dictionary updated with {'geopackage_region_gdf': [{'gdf':, 'gdf_simplified':, 'spatial_index':, 'spatial_index':, 'minx':, 'miny', 'maxx', 'maxy', 'empty'}]}
    - Prints to console a note when the geodatabase region mapping does not work. Stores in log.txt a list of regions that couldn't be mapped.
    
    deposit.resource_discovery()
    - geodataframe keyword argument removed (now embedded within f data structure)
    - Coordinate generate logic moved to spatial.generate_region_coordinate() and made more elegant

--- modules/spatial.py
+++ modules/spatial.py
@@ -4,81 +4,105 @@ Module with functions for handling spatial data
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
-from shapely.ops import nearest_points
+from shapely.strtree import STRtree  # Spatial index for faster querying
+from shapely.prepared import prep
 import random
 
 
 def import_geopackage(path):
     """
-    Reads a geopackage (.shp) at 'shapefile_path' and returns a geopandas dataframe
+    Reads a geopackage (.shp) at 'path' and returns a geopandas dataframe
     """
     try:
-        # Load the shapefile
+        # Load the geopackage
         gdf = gpd.read_file(path)
         return gdf
     except Exception as e:
-        print(f"Failed to load shapefile: {e}")
+        print(f"Failed to load geopackage: {e}")
         return None
 
-def generate_region_coordinate(geodataframe, region_label, region_value, method='random', log_path='None'):
+def gdf_region_preprocess(geodataframe, simplify=False):
+
+    if geodataframe.empty:
+        gdf_dict = {'gdf': geodataframe,
+                    'gdf_simplified': geodataframe,
+                    'spatial_index': None,
+                    'minx': None,
+                    'miny': None,
+                    'maxx': None,
+                    'maxy': None,
+                    'empty': True}
+    else:
+        if len(geodataframe) > 1:
+            region = geodataframe.unary_union
+        else:
+            region = geodataframe.geometry.iloc[0]
+
+        if simplify:
+            # Simplify the geometry to speed up point containment check (optional)
+            # Defaulting to False as prepared_region conversion seems to be sufficient with test datasets
+            simplified_region = region.simplify(tolerance=0.001, preserve_topology=True)
+        else:
+            simplified_region = region
+
+        # Build a spatial index to reduce the number of containment checks
+        spatial_index = STRtree([simplified_region])
+
+        # Generate region bounds
+        minx, miny, maxx, maxy = region.bounds
+
+        gdf_dict = {'gdf': geodataframe,
+                    'gdf_simplified': simplified_region,
+                    'spatial_index': spatial_index,
+                    'minx': minx,
+                    'miny': miny,
+                    'maxx': maxx,
+                    'maxy': maxy,
+                    'empty': False}
+
+    return gdf_dict
+
+def prepare_gdf(gdf_dict):
+    # For use in pemmss.scenario() as prepared regions can't be pickled, so don't work with multiprocessing or deepcopy
+    # Prepare the region to enable faster, cached containment checks
+    return_list = []
+    for i in gdf_dict:
+        prepared_region = prep(i['gdf_simplified'])
+        return_list.append(prepared_region)
+
+    return return_list
+
+def generate_region_coordinate(gdf_dict, gdf_prepared):
     """
-    Selects a region from a shapefile and generates latitude and longitude coordinates as decimal degrees.
+    Generates a region coordinate within polygons of a geodataframe and generates latitude and longitude coordinates as decimal degrees. This will typically be filtered prior to this function call.
 
     Parameters:
-    - geodataframe (geopandas dataframe): The GeoDataFrame containing the geopackage data.
-    - region_label (str): The attribute used to identify the region (e.g., a column name such as REGION).
-    - region_value (str): The value of the region_label to filter the desired region (e.g. 'Region 1', or 'Chile', or 'Asia').
-    - method (str): 'midpoint' to return the spatial midpoint, 'random' to return a random coordinate within the region.
+    - gdf_dict containing a geodatabase and preprocessed spatial_index (STRtree), bounds
 
     Returns:
     - (float, float): Latitude and longitude coordinates as decimal degrees. Returns (None, None) if region_label or region_value not in geodataframe.
 
     Example use:
-    lat, lon = generate_region_coordinate(shapefile_gdf, region_label, region_value, method)
+    lat, lon = generate_region_coordinate(geodataframe, region_label, region_value, method)
     """
-
-    if region_label not in geodataframe:
+    if gdf_dict['empty']:
         return None, None
-
-    # Select the region based on the provided label and value
-    region = geodataframe[geodataframe[region_label] == region_value]
-
-    if region.empty:
-        return None, None
-
-    # Ensure the region is in a single geometry
-    if len(region) > 1:
-        region = region.unary_union
     else:
-        region = region.geometry.iloc[0]
+        prepared_region = gdf_prepared
+        spatial_index = gdf_dict['spatial_index']
+        minx = gdf_dict['minx']
+        miny = gdf_dict['miny']
+        maxx = gdf_dict['maxx']
+        maxy = gdf_dict['maxy']
 
-    if method == 'midpoint':
-        # Calculate the centroid (midpoint)
-        centroid = region.centroid
-        return centroid.y, centroid.x
-    elif method == 'random':
-        # Generate a random point strictly within the polygon
-        minx, miny, maxx, maxy = region.bounds
+        # Try generating a random point within the bounds
+        # for _ in range(100):  # Reduce the number of attempts if needed
         while True:
             random_point = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
-            if region.contains(random_point):
+
+            # Only check if the point is within the polygon using the spatial index
+            if any(prepared_region.contains(random_point) for r in spatial_index.query(random_point)):
                 return random_point.y, random_point.x
-    elif method == 'centroid_within':
-        # Calculate the centroid
-        centroid = region.centroid
-        if region.contains(centroid):
-            return centroid.y, centroid.x
-        else:
-            # Find the nearest point within the polygon
-            nearest_point = nearest_points(region, centroid)[0]
-            return nearest_point.y, nearest_point.x
-    else:
-        raise ValueError("Method must be 'midpoint', 'random', or 'centroid_within'")
-
-
-# [BM] The following function was written to remove the save_project_as_shapefile from the iteration loop, and out to the post-processing loop
-# It reads from the scenarios/iterations and generates 1 shapefile per Scenario (e.g. Decoupling). This shapefile will have j layers, where j is the number of iterations.
-# Each layer (j) will have a cloud of points respective to the Projects (i)
 
 def deduplicate_columns(columns):
     """
@@ -89,19 +113,23 @@ def deduplicate_columns(columns):
     for i, col in enumerate(columns):
         if col in seen:
             seen[col] += 1
-            columns[i] = f"{col[:9]}_{seen[col]}"  # Keep names within 10 characters
+            columns[i] = f"{col}_{seen[col]}"
         else:
             seen[col] = 0
     return columns
 
-def save_scenario_geopackage(shapefile_gdf, scenario_folders):
+# [BM] The following function was written to remove the geopackage generation from the iteration loop and out to the post-processing loop
+# It reads from the scenarios/iterations and generates 1 geopackage per Scenario (e.g. Decoupling). This geopackage will have j layers, where j is the number of iterations.
+# Each layer (j) will have a cloud of points respective to the Projects (i)
+
+def save_scenario_geopackage(geodataframe, scenario_folders):
     """
-    Creates and saves an OGC geopackage for each scenario with data from all iterations combined.
+    Creates and saves an OGC geopackage for each scenario (i) with data from all iterations (j) combined.
 
     Files written: [scenario_folder]/_geopackage.gpkg
 
     Parameters:
-    - shapefile_gdf (GeoDataFrame or None): The original GeoDataFrame from the shapefile. Can be None.
+    - geodataframe (GeoDataFrame or None): The original GeoDataFrame from the input_geopackage.gpkg. Can be None.
     - scenario_folders (list): List of paths to scenario folders.
     """
     import warnings
@@ -124,8 +152,8 @@ def save_scenario_geopackage(shapefile_gdf, scenario_folders):
             projects_gdf = gpd.GeoDataFrame(projects_df, geometry=geometry)
 
             # Set CRS to match the input shapefile if provided
-            if shapefile_gdf is not None:
-                projects_gdf.set_crs(shapefile_gdf.crs, inplace=True)
+            if geodataframe is not None:
+                projects_gdf.set_crs(geodataframe.crs, inplace=True)
             else:
                 projects_gdf.set_crs("EPSG:4326", inplace=True)  # Default CRS (WGS 84)
 
@@ -138,13 +166,13 @@ def save_scenario_geopackage(shapefile_gdf, scenario_folders):
         # Combine all the points from different iterations into a single GeoDataFrame
         combined_gdf = gpd.GeoDataFrame(pd.concat(all_points_gdfs, ignore_index=True))
 
-        # Deduplicate column names to ensure uniqueness and keep them within 10 characters
+        # Deduplicate column names to ensure uniqueness
         combined_gdf.columns = deduplicate_columns(combined_gdf.columns.to_list())
 
         # Define the output path for the OGC GeoPackage
         output_geopackage_path = scenario_folder / f'_geopackage.gpkg'
 
-        # Save the shapefile
+        # Save the geopackage
         try:
             combined_gdf.to_file(output_geopackage_path)
             output_paths.append(output_geopackage_path)
